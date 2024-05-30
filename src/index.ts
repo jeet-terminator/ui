fc729e92eacc479d3a77ce0ceb5e5275e17b9445
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-05-30 22:18:21 +0200

    update

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import WebsiteBackground from './ressources/Fond.svg';
+import { html , render } from "@lithium-framework/core-dom";
+import { useStyle , unsafeSVG } from "@lithium-framework/core-dom/directives";
+
+render( html`<div 
+  style = ${useStyle({ 
+    position : "absolute", 
+    height : "100%",
+    width : '100%',
+    top : 0,
+    left : 0,
+    overflow : "hidden"
+  })} 
+>
+  <div>${unsafeSVG(WebsiteBackground)}</div>
+</div>` , document.body );
