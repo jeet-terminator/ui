3d2a34a93213ee4c74c61eb9c32fab00d8def5a4
Houthoofd <houthoofd.benoit48@gmail.com>
2024-05-31 15:39:12 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import WebsiteBackground from './ressources/Fond.svg';
 import { html , render } from "@lithium-framework/core-dom";
 import { useStyle , unsafeSVG } from "@lithium-framework/core-dom/directives";
+import { popUpWindow } from './components';
 
-render( html`<div 
+render( html`<div
   style = ${useStyle({ 
     position : "absolute", 
     height : "100%",
@@ -12,5 +13,6 @@ render( html`<div
     overflow : "hidden"
   })} 
 >
+  ${popUpWindow()}
   <div>${unsafeSVG(WebsiteBackground)}</div>
 </div>` , document.body );
