7764d8037d06022579c27d659314fe500c04e4a2
Guillaume Houthoofd <47045327+MortallicaXxX@users.noreply.github.com>
2024-05-31 21:45:14 +0200

    update

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,2 @@
-export {};
+import './index.css';
+import './router';
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,22 @@ import { html , render } from "@lithium-framework/core-dom";
 import { useStyle , unsafeSVG } from "@lithium-framework/core-dom/directives";
 import { popUpWindow } from './components';
 
-render( html`<div
-  style = ${useStyle({ 
-    position : "absolute", 
-    height : "100%",
-    width : '100%',
-    top : 0,
-    left : 0,
-    overflow : "hidden"
-  })} 
->
-  ${popUpWindow()}
-  <div>${unsafeSVG(WebsiteBackground)}</div>
-</div>` , document.body );
+import './index.css';
+import './router';
+
+render( html`<main-application></main-application>` , document.body );
+
+// render( html`<div
+//   style = ${useStyle({ 
+//     position : "absolute", 
+//     height : "100%",
+//     width : '100%',
+//     top : 0,
+//     left : 0,
+//     overflow : "hidden"
+//   })} 
+// >
+//   ${popUpWindow()}
+//   <div>${unsafeSVG(WebsiteBackground)}</div>
+// </div>` , document.body );
+
